cd2203836fd6f3048649f306e83957e87132256b
axell.tielemans <axelltielemans@gmail.com>
2024-12-23 22:10:02 +0100

    restart of project

--- scenes/character/player_char.gd
+++ scenes/character/player_char.gd
@@ -1,15 +1,25 @@
 extends CharacterBody2D
-@export var move_speed : float = 100
 
-func _physics_process(_delta):
-	var input_direction = Vector2(
-	Input.get_action_strength("right") - Input.get_action_strength("left"),
-	Input.get_action_strength("down") - Input.get_action_strength("up"),
-	)
-	
-	
-	print(input_direction);
-	
-	velocity = input_direction * move_speed
-	
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
 	move_and_slide()
