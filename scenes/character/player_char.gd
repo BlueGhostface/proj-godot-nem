2fd55482ca9e9d82c6c3280ffab017028fc62d04
axell.tielemans <axelltielemans@gmail.com>
2024-12-24 20:55:50 +0100

    update tilemap and character move

--- scenes/character/player_char.gd
+++ scenes/character/player_char.gd
@@ -1,8 +1,8 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+@export var SPEED : float = 300.0
+@export var jump_velocity : float = -400.0
 
 
 func _physics_process(delta: float) -> void:
@@ -12,7 +12,7 @@ func _physics_process(delta: float) -> void:
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		velocity.y = jump_velocity
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
