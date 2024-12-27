12a30d514f939979e65a9a1ced71ec5a4dd54eed
axell.tielemans <axelltielemans@gmail.com>
2024-12-27 22:53:54 +0100

    jump animation

--- scenes/character/player_char.gd
+++ scenes/character/player_char.gd
@@ -11,19 +11,26 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var has_double_jumped : bool = false
 var animation_locked : bool = false
 var direction : Vector2 = Vector2.ZERO
+var was_in_air : bool = false
 
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		was_in_air = true
 	else:
 		has_double_jumped = false
+		
+		if was_in_air == true:
+			land()
+		
+		was_in_air = false
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump"):
 		if is_on_floor():
-			velocity.y = jump_velocity
+			jump()
 		elif not has_double_jumped:
 			velocity.y = double_jump_velocity
 			has_double_jumped = true
@@ -34,7 +41,7 @@ func _physics_process(delta: float) -> void:
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	direction = Input.get_vector("left", "right","up","down")
 	
-	if direction:
+	if direction.x !=0  && animated_sprite.animation != "jump_end":
 		velocity.x = direction.x * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
@@ -44,18 +51,35 @@ func _physics_process(delta: float) -> void:
 	update_facing_direction()
 	
 	
+func land():
+	animation_locked = true
+	animated_sprite.play("jump_end")
 
+func jump():
+	velocity.y = jump_velocity
+	animation_locked = true
+	animated_sprite.play("jump_start")
 
 func update_animation():
 	if not animation_locked:
-		if direction.x != 0:
-			animated_sprite.play("run")
-			
+		if not is_on_floor():
+			animated_sprite.play("jump_loop")
 		else:
-			animated_sprite.play("idle")
+			if direction.x != 0:
+				animated_sprite.play("run")
+				
+			else:
+				animated_sprite.play("idle")
 
 func update_facing_direction():
 	if direction.x > 0:
 		animated_sprite.flip_h = false
 	elif direction.x < 0:
 		animated_sprite.flip_h = true
+
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	if animated_sprite.animation == "jump_end":
+		animation_locked = false
+	elif(animated_sprite.animation == "jump_start"):
+		animation_locked = false
